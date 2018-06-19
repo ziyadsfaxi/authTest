70bf205f651978a326785058a55df96df3e6afec
Ziyad Sfaxi <ziyadsfaxi@gmail.com>
2018-06-19 16:13:21 +0800

    fixed edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -22,17 +22,17 @@
 
                 <div class="form-group">
                     {{ Form::label('name', 'Name') }}
-                    {{ Form::text('name', null, ['class' => 'form-control']) }}
+                    {{ Form::text('name', $user->name, ['class' => 'form-control']) }}
                 </div>
 
                 <div class="form-group">
                     {{ Form::label('email', 'Email') }}
-                    {{ Form::email('email', null, ['class' => 'form-control']) }}
+                    {{ Form::email('email', $user->email, ['class' => 'form-control']) }}
                 </div>
 
                 <div class="form-group">
                     {{ Form::label('phone', 'Phone') }}
-                    {{ Form::text('phone', null, ['class' => 'form-control']) }}
+                    {{ Form::text('phone', $user->phone, ['class' => 'form-control']) }}
                 </div>
 
                 <div class="form-group">
